a674f9a3a8dbae22c3681921f40259573bc8c982
Felipe Bidu <felipe@felipevr.com>
2017-05-08 13:59:19 -0300

    Linting and adding readme

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # -*- encoding: utf-8 -*-
-
+"""
+The main module
+"""
 import utils
 
 
 def main():
     with open('./kinases.txt') as f:
         for line in f:
-            gene=line.rstrip()
-            html_text=utils.search_orphanet(gene)
-            result=utils.get_first_result(html_text)
+            gene = line.rstrip()
+            html_text = utils.search_orphanet(gene)
+            result = utils.get_first_result(html_text)
             if result != '':
-                teste=''.join([linha.strip() for linha in result.strip().splitlines()])
+                teste = ''.join([
+                        linha.strip() for linha in result.strip().splitlines()
+                    ])
                 print(gene+"\thttp://www.orpha.net/consor/cgi-bin/"+teste)
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,23 +1,27 @@
 # -*- encoding: utf-8 -*-
+"""
+Utilitary functions do download and parse data from Orpha net
+"""
 from requests import post
 from bs4 import BeautifulSoup
+
 
 def search_orphanet(keyword, search_type='Gen'):
     """
     Returns the HTML of a Orphanet search. By default,
     it searches for a gene
     """
-    search_url = "http://www.orpha.net/consor/cgi-bin/Disease_Search_Simple.php?lng=EN"
+    search_url = r"http://www.orpha.net/consor/cgi-bin/Disease_Search_Simple.php?lng=EN"
     search_data = {
             'Disease_Disease_Search_diseaseGroup': keyword,
             'Disease_Disease_Search_diseaseType': search_type
             }
 
-    r = post(search_url, search_data)
-    if r.status_code == 200:
-        return r.text
+    request = post(search_url, search_data)
+    if request.status_code == 200:
+        return request.text
     else:
-        raise Exception("Search failed with status {}".format(r.status_code))
+        raise Exception("Search failed with status {}".format(request.status_code))
 
 
 def get_first_result(orphanet_search):
